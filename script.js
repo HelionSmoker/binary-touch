53937861447f2ef5803f646d7fd56d218249fa11
HelionSmoker <gritcodragos04@gmail.com>
2023-12-24 18:48:42 +0200

    Complete version 1.0.0 - Change Braille pattern from col-by-col to row-by-row - Link the 'styles.css' file in 'index.html' - Create a pattern container - Create a result container - Create a buttons container - Wire up the buttons to miscellaneous functions - Create a Pattern class that holds pretty much all the logic - Update the 'copy' function to output clearer messages

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
 import { BRAILLE_SYMBOLS } from "./data.js";
-function convertPattern(pattern, symbols) {
-    pattern = pattern.padEnd(8, '0');
-    return symbols[parseInt(pattern, 2)];
+const PATTERN_CONTAINER = document.getElementById("pattern-container");
+const RESULT_CHAR_CONTAINER = document.getElementById("result-char-container");
+class Pattern {
+    constructor() {
+        this.patternNode = document.createElement("div");
+        this.patternNode.classList.add("pattern");
+        this.cells = [];
+        this.createCellNodes();
+        this.removeNode = this.createRemoveButton();
+        this.patternNode.appendChild(this.removeNode);
+        PATTERN_CONTAINER.appendChild(this.patternNode);
+        this.resultCharNode = this.createResultNode();
+        RESULT_CHAR_CONTAINER.appendChild(this.resultCharNode);
+    }
+    createCellNodes() {
+        for (let i = 0; i < 8; i++) {
+            const cell = document.createElement("button");
+            cell.classList.add("cell");
+            cell.onclick = () => {
+                cell.classList.toggle("active");
+                this.updateValue();
+                this.updateResult();
+            };
+            this.cells.push(cell);
+            this.patternNode.appendChild(cell);
+        }
+    }
+    createResultNode() {
+        const resultCharNode = document.createElement("span");
+        resultCharNode.classList.add("result-char");
+        resultCharNode.textContent = "⠀"; // empty braille char
+        return resultCharNode;
+    }
+    createRemoveButton() {
+        const removeNode = document.createElement("button");
+        removeNode.classList.add("remove");
+        removeNode.textContent = "X";
+        removeNode.title = "Remove Pattern";
+        removeNode.onclick = () => this.remove();
+        return removeNode;
+    }
+    convertPattern(pattern, symbols) {
+        return symbols[parseInt(pattern, 2)];
+    }
+    updateValue() {
+        const pattern = this.cells
+            .map((button) => {
+            return button.classList.contains("active") ? "1" : "0";
+        })
+            .join("");
+        this.value = this.convertPattern(pattern, BRAILLE_SYMBOLS);
+    }
+    updateResult() {
+        this.resultCharNode.textContent = this.value;
+    }
+    remove() {
+        PATTERN_CONTAINER.removeChild(this.patternNode);
+        RESULT_CHAR_CONTAINER.removeChild(this.resultCharNode);
+        if (PATTERN_CONTAINER.children.length === 0) {
+            addPattern();
+        }
+    }
 }
-console.log(convertPattern('101', BRAILLE_SYMBOLS));
+function copy(text) {
+    navigator.clipboard.writeText(text).then(function () {
+        console.log(`Copied: '${text}'`);
+    }, function (err) {
+        console.error("Could not copy text: ", err);
+    });
+}
+function addPattern() {
+    new Pattern();
+}
+function copyResult() {
+    const result = Array.from(RESULT_CHAR_CONTAINER.children)
+        .map((charNode) => charNode.textContent)
+        .join("");
+    copy(result);
+}
+window.addPattern = addPattern;
+window.copyResult = copyResult;
+addPattern();
